5708c4c0474b6e0d95b8b593f77379eea9a9b8bd
Jhon Lloyd F. Omblero <113282952+joxyle-jhon04@users.noreply.github.com>
2023-11-25 13:04:35 +0800

    Modified

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,11 +1,33 @@
+import { useReducer } from 'react'
 import './App.css'
 
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand} || ""${payload.digit}`
+      }
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] =useReducer(reducer, {})
+
+  dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
   return (
     <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'></div>
-        <div className='current-operand'></div>
+        <div className='previous-operand'> {previousOperand} {operation} </div>
+        <div className='current-operand'> {currentOperand} </div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
---
+++ Calculator/src/DigitButton.jsx
@@ -0,0 +1,4 @@
+export default function DigitButton ({ dispatch,digit }){
+    return  <button>1</button>
+ 
+}
